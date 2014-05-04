4cefd1f285aa43691c187880cb4936ed34bed796
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2014-05-03 22:06:58 -0500

    Add simple CRUD examples

--- Source/SalesForceApiTutorial.View.Console/SalesForceController.cs
+++ Source/SalesForceApiTutorial.View.Console/SalesForceController.cs
@@ -2,6 +2,7 @@
 using Salesforce.Force;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,30 @@ namespace SalesForceApiTutorial.View.Console
             await auth.UsernamePasswordAsync(consumerKey, consumerSecret, userName, password + passwordSecurityToken);
 
             var forceClient = new ForceClient(auth.InstanceUrl, auth.AccessToken, auth.ApiVersion);
+
+
+            // Insert some test data
+            dynamic myAccount = new ExpandoObject();
+            myAccount.Name = "Aaron";
+            var accountId = await forceClient.CreateAsync("Account", myAccount);
+
+
+            // Query via API
+            var accounts = await forceClient.QueryAsync<dynamic>("select Id, Name from Account");
+            
+            foreach (var account in accounts.records)
+            {
+                System.Console.WriteLine(String.Format("Id:{0}, Name:{1}", account.Id, account.Name));
+            }
+
+            var accountResult = await forceClient.QueryByIdAsync<dynamic>("Account", accountId);
+
+
+            // Modify via API
+            myAccount.Name = "Aaron 2";
+            await forceClient.UpdateAsync("Account", accountId, myAccount);
+
+            await forceClient.DeleteAsync("Account", accountId);
         }
     }
 }
